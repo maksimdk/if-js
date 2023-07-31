9710cb116b2ab408e2064c7d90ef39e9a7ca0d13
maksim <maksimkot.work@gmail.com>
2023-07-31 21:30:03 +0300

    Update fetch hotels

--- src/js/homesSection.js
+++ src/js/homesSection.js
@@ -1,3 +1,13 @@
+fetch('https://if-student-api.onrender.com/api/hotels/popular')
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error(`${response.status} - ${response.statusText}`);
+    }
+    return response.json();
+  })
+  .then((json) => displayContent(json))
+  .catch((err) => console.log(err.message));
+
 export function createCard(imageUrl, name, city, country) {
   return `
   <img
@@ -12,17 +22,17 @@ export function createCard(imageUrl, name, city, country) {
 
 export function displayContent(arr) {
   const element = document.querySelector('.homes__cards');
-  arr.forEach((item) => {
+  for (let item = 0; item < 4; item++) {
     const newEl = document.createElement('li');
     newEl.classList.add('col-lg-3', 'col-md-6', 'col-sm-3', 'homes__card');
     element.append(newEl);
     newEl.innerHTML = createCard(
-      item.imageUrl,
-      item.name,
-      item.city,
-      item.country,
+      arr[item].imageUrl,
+      arr[item].name,
+      arr[item].city,
+      arr[item].country,
     );
-  });
+  }
 
   const newBtn = document.createElement('button');
   newBtn.classList.add('btn', 'arrow-block', 'homes__arrow-block');
@@ -33,8 +43,3 @@ export function displayContent(arr) {
       </svg>
   `;
 }
-
-fetch('https://if-student-api.onrender.com/api/hotels/popular')
-  .then((response) => response.json())
-  .then((json) => displayContent(json))
-  .catch((err) => console.log(err));
