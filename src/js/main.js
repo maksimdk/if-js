edea9a8a61094bb5a5602422dcfac4d3ef033277
maksim <maksimkot.work@gmail.com>
2023-07-31 20:53:18 +0300

    created fetch

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,8 @@
-import { data } from './homesSection.js';
+//import { data } from './homesSection.js';
 import { displayContent } from './homesSection.js';
 
 import { showFilter } from './filterHotel.js';
 
 document.addEventListener('click', showFilter);
 
-displayContent(data);
+//displayContent(data);
