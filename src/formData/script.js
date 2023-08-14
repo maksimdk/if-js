5b026ebb17584589db20b1b7a8a7a2fa69e02df9
maksim <maksimkot.work@gmail.com>
2023-08-14 21:38:25 +0300

    added sending file to server

--- src/formData/script.js
+++ src/formData/script.js
@@ -1,3 +1,27 @@
 const formEl = document.getElementById('form');
 
-console.log(formEl);
+formEl.addEventListener('submit', async (event) => {
+  event.preventDefault();
+
+  const body = new FormData(formEl);
+
+  const fetchOptions = {
+    method: 'POST',
+    body,
+  };
+
+  const res = await fetch(
+    'https://if-student-api.onrender.com/api/file',
+    fetchOptions,
+  )
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(response.statusText);
+      }
+      return response.json();
+    })
+    .then((result) => result)
+    .catch((error) => console.log(error.message));
+
+  console.log(res);
+});
