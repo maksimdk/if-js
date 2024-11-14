56ed0288fd71432f0145fa75065a6514042a687d
maksim <maksimkot.work@gmail.com>
2024-11-14 20:59:32 +0300

    Added docs directory for GitHub Pages

--- src1/lesson-4/scripts/sum.js
+++
@@ -1,5 +0,0 @@
-export const sum = (a) => {
-  return (b) => {
-    return a + b;
-  };
-};
--- src1/lesson-4/tests/sum.test.js
+++
@@ -1,7 +0,0 @@
-import { sum } from '../scripts/sum';
-
-describe('sum of two values', () => {
-  test('sum a & b', () => {
-    expect(sum(5)(3)).toBe(8);
-  });
-});
